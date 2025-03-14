688dd366dbfd2164913bebcf70121345c7f52fe0
RugvedGadakh <rugvedgadakh12@gmail.com>
2025-03-14 22:10:29 +0530

    frontend

---
+++ data/candidatesMenuData.js
@@ -0,0 +1,37 @@
+module.exports = [
+  // {
+  //   id: 1,
+  //   name: "Dashboard",
+  //   icon: "la-home",
+  //   routePath: "/candidates-dashboard/dashboard",
+  //   active: "active",
+  // },
+  {
+    id: 1,
+    name: "My Profile",
+    icon: "la-user-tie",
+    routePath: "/candidates-dashboard/my-profile",
+    active: "",
+  },
+  {
+    id: 2,
+    name: "Resume Builder",
+    icon: "la la-file-invoice",
+    routePath: "/resumebuilder",
+    active: "",
+  },
+  {
+    id: 3,
+    name: "Logout",
+    icon: "la-sign-out",
+    routePath: "/login",
+    active: "",
+  },
+  {
+    id: 4,
+    name: "Delete Profile",
+    icon: "la-trash",
+    routePath: "/",
+    active: "",
+  },
+];
